26ad19eb7762bf99f19a2c95d5994b5749b3d18e
nirvanaGarciaV <nirvagarciav@gmail.com>
2024-01-19 11:57:44 -0500

    fixing skills padding

---
+++ src/auxiliars/MyConsts.js
@@ -0,0 +1,179 @@
+const skillsData = [
+    {
+        name: "React",
+        image: "assets/react.png",
+        type: "Frameworks",
+    },
+    {
+        name: "Angular",
+        image: "assets/angular.png",
+        type: "Frameworks",
+    },
+    {
+        name: "Vue.js",
+        image: "assets/vue.png",
+        type: "Frameworks",
+    },
+    {
+        name: "JavaScript",
+        image: "assets/javascript.png",
+        type: "Languages",
+    },
+    {
+        name: "TypeScript",
+        image: "assets/typescript.png",
+        type: "Languages",
+    },
+    {
+        name: "GitHub",
+        image: "assets/github.png",
+        type: "Tools",
+    },
+    {
+        name: "Git",
+        image: "assets/git.png",
+        type: "Tools",
+    },
+    {
+        name: "C#",
+        image: "assets/csharp.png",
+        type: "Languages",
+    },
+    {
+        name: "MySQL",
+        image: "assets/mysql.png",
+        type: "Databases",
+    },
+    {
+        name: "PostgreSQL",
+        image: "assets/postgresql.png",
+        type: "Databases",
+    },
+    {
+        name: "Azure",
+        image: "assets/azure.png",
+        type: "Cloud",
+    },   
+    {
+        name: "Figma",
+        image: "assets/figma.png",
+        type: "Tools",
+    },
+    {
+        name: "Spring Boot",
+        image: "assets/springboot.png",
+        type: "Tools",
+    },
+    {
+        name: "Postman",
+        image: "assets/postman.png",
+        type: "Tools",
+    },
+    {
+        name: "MongoDB",
+        image: "assets/mongodb.png",
+        type: "Databases",
+    },
+    {
+        name: "Python",
+        image: "assets/python.png",
+        type: "Languages",
+    },
+    {
+        name: "Java",
+        image: "assets/java.png",
+        type: "Languages",
+    },   
+];
+  
+const projects = [  
+    {
+        title:"Mappi - GMaps API",
+        cardimage: "/assets/maps.png",
+        desc: "GoogleMaps API integration, using ReactJS",
+        techStack: "React.js, Javascript, NodeJS",
+        type: "API",
+        open: "https://googlemaps-api.vercel.app/"
+    },
+    {
+        title:"Pangea - Streaming Platform",
+        cardimage: "/assets/pangea.png",
+        desc: "Studio Ghibli Streaming platform, using ReactJS",
+        techStack: "React.js, Typescript, NodeJS",
+        type: "Frontend",
+        link: "https://github.com/nirvagarcia/pangea-ghibli",
+        open: "https://pangea-ghibli.vercel.app/"
+    },
+    {
+        title:"My Portfolio",
+        cardimage: "/assets/portfolio_nirvana.png",
+        desc: "Building my own portfolio",
+        techStack: "React.js, Javascript",
+        type: "Frontend",
+        link: "https://github.com/nirvagarcia/portafolio-nirvana-v2",
+        open: "https://nirvana-garcia.vercel.app/"
+    },
+    {
+        title:"AKIRA E-Commerce",
+        cardimage: "/assets/akira.png",
+        desc: "Asian entertainment E-Commerce, using AngularJS",
+        techStack: "Angular.js, MySQL, Javascript",
+        type: "Fullstack",
+        link: "https://github.com/nirvagarcia/pangea-ghibli",
+        open: "https://pangea-ghibli.vercel.app/"
+    },
+    {
+        title:"Financial Tool (MiVivienda)",
+        cardimage: "/assets/mivivienda.png",
+        desc: "'Mi Vivienda' Financial simulator, using VueJS",
+        techStack: "Vue.js, Node.js, MySQL, Javascript",
+        type: "Fullstack",
+        link: "https://github.com/nirvagarcia/pangea-ghibli",
+        open: "https://pangea-ghibli.vercel.app/"
+    },   
+    {
+        title:"Doctify",
+        cardimage: "/assets/doctify.png",
+        desc: "Platform for contacting doctors, booking appointments",
+        techStack: "React.js, MySQL, Javascript",
+        type: "Fullstack",
+        link: "https://github.com/nirvagarcia/pangea-ghibli",
+        open: "https://pangea-ghibli.vercel.app/"
+    },
+    {
+        title:"WeatherApp - OpenWeatherMap API",
+        cardimage: "/assets/weatherapi.gif",
+        desc: "OpenWeatherMap API integration, using Vue.js",
+        techStack: "Vue.js, Javascript, NodeJS",
+        type: "API",
+        open: "https://googlemaps-api.vercel.app/"
+    },
+    {
+        title:"Manicom - TMDB API",
+        cardimage: "/assets/movie_api.png",
+        desc: "TMDB API integration, using AngularJS",
+        techStack: "Angular.js, Javascript, NodeJS",
+        type: "API",
+        open: "https://googlemaps-api.vercel.app/"
+    },     
+    {
+        title:"Tribute Page - Beabadoobe",
+        cardimage: "/assets/bea.png",
+        desc: "Biographical page of Beabadoobe, 23 year old Filipino singer and songwriter.",
+        techStack: "Vue.js, Node.js, MySQL, Javascript",
+        type: "Frontend",
+        link: "https://github.com/nirvagarcia/pangea-ghibli",
+        open: "https://pangea-ghibli.vercel.app/"
+    },
+    {
+        title:"AKIRA Mobile!",
+        cardimage: "/assets/akiramobile.png",
+        desc: "Asian entertainment E-Commerce, using AngularJS",
+        techStack: "Flutter Mobile",
+        type: "Mobile",
+        link: "https://github.com/nirvagarcia/pangea-ghibli",
+        open: "https://pangea-ghibli.vercel.app/"
+    },   
+];
+
+export { skillsData, projects };  
